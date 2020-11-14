842f4c62cfad0e42ed2fe9b95bb6faa5488202fe
pedroso1001 <37347554+pedroso1001@users.noreply.github.com>
2020-11-14 14:03:19 +0000

    Add files via upload

---
+++ part2/Task_2.m
@@ -0,0 +1,60 @@
+load('data1.mat');
+
+%Amouns_rof inpus_rfeatures%
+[n,K] = size(X);
+
+%Stopping Criteria%
+s0 = -ones(1,n);
+r0 = 0;
+epsilon = 10^(-6);
+s_r0 = [s0 r0]';
+A = [X; -ones(K,1)'];
+
+
+%For the Backtracking Subroutine%
+alpha0 = 1;
+gama = 10^(-4);
+beta = 0.5;
+gradients=[];
+
+
+s_r= s_r0;
+alpha = alpha0;
+while (1)
+    g_k = gradient_function(s_r, A ,Y,K);
+    if norm(g_k) < epsilon
+        break;
+    end
+    d = -g_k;
+    alpha = alpha0;
+    while minimize_function(s_r+ alpha.*d, A, Y, K) >= minimize_function(s_r, A ,Y,K) + (gama.*g_k'*(alpha.*d))
+        alpha = beta .* alpha;
+    end
+    s_r= s_r+ (alpha .* d);
+    gradients = [gradients norm(g_k)];
+end
+
+figure;
+semilogy(gradients);
+grid on;
+
+figure;
+for i=1:K
+   if Y(i) == 0
+        a = scatter(X(1, i), X(2, i), [], 'red','LineWidth',1);
+        hold on
+    else
+        b = scatter(X(1, i), X(2, i), [], 'blue','LineWidth',1);
+        hold on
+    end
+       
+end
+
+x = linspace(-3,7);
+y = s_r(3)/s_r(2) - x*(s_r(1)/s_r(2));
+c = plot(x, y, '--y','Color','g', 'LineWidth', 2);
+%a, b and c so thas_ris_rskips the K scatters and legends the line
+legend([a(1) b(1) c(1)], 'Y = 0', 'Y = 1', 's^Tx = r') 
+title('Datases_r1')
+xlabel('x_1');
+ylabel('x_2');
---
+++ part2/Task_3.m
@@ -0,0 +1,56 @@
+load('data2.mat');
+
+%Amouns_rof inpus_rfeatures%
+[n,K] = size(X);
+
+%Stopping Criteria%
+s0 = -ones(1,n);
+r0 = 0;
+epsilon = 10^(-6);
+s_r0 = [s0 r0]';
+A = [X; -ones(K,1)'];
+
+
+%For the Backtracking Subroutine%
+alpha0 = 1;
+gama = 10^(-4);
+beta = 0.5;
+gradients=[];
+
+
+s_r= s_r0;
+alpha = alpha0;
+while norm(gradient_function(A ,Y ,s_r, K)) >= epsilon
+    d = -gradient_function(A ,Y ,s_r, K);
+    alpha = alpha0;
+    while minimize_function(s_r+ alpha.*d, A, Y, K) >= minimize_function(s_r, A ,Y,K) + (gama.*gradient_function(A ,Y ,s_r, K)'*(alpha.*d))
+        alpha = beta .* alpha;
+    end
+    s_r= s_r+ (alpha .* d);
+    gradients = [gradients norm(gradient_function(A ,Y ,s_r, K))];
+end
+
+figure;
+semilogy(gradients);
+grid on;
+
+figure;
+for i=1:K
+   if Y(i) == 0
+        a = scatter(X(1, i), X(2, i), [], 'red','LineWidth',1);
+        hold on
+    else
+        b = scatter(X(1, i), X(2, i), [], 'blue','LineWidth',1);
+        hold on
+    end
+       
+end
+
+x = linspace(-3,7);
+y = s_r(3)/s_r(2) - x*(s_r(1)/s_r(2));
+c = plot(x, y, '--y','Color','g', 'LineWidth', 2);
+%a, b and c so thas_ris_rskips the K scatters and legends the line
+legend([a(1) b(1) c(1)], 'Y = 0', 'Y = 1', 's^Tx = r') 
+title('Datases_r1')
+xlabel('x_1');
+ylabel('x_2');
